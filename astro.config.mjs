8f674f6666201ecc3aa5fbbe933324bafd7eab3d
Tony <424635328@qq.com>
2026-05-23 12:13:23 +0800

    2026/05/23 12:13:23.29

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   site: process.env.URL || 'http://localhost:4321',
 });
